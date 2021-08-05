ef383b05eb99bbe1697b752d299be1582ef58eae
Emma Carpenetti <emmac4164@gmail.com>
2021-08-04 23:22:28 -0400

    Initial Commit

---
+++ The Color Guard App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Color Guard App
+//
+//  Created by Emma Carpenetti on 8/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
